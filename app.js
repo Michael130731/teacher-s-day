8cb45151c081ca44633b6e0c1d7498b23d28c005
Michael130731 <jinpyo8396@gmail.com>
2025-05-12 20:00:08 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let diff = 1000
 
 function updateTimer() {
     if (0 < diff) {
-        const future = Date.parse("2025/05/09 20:15:00");
+        const future = Date.parse("2025/05/15 09:00:00");
         const now = new Date();
         diff = future - now;
         document.getElementById("timer").innerHTML = '<div>' + Math.floor(diff / (1000 * 60 * 60 * 24)) + '<span>Days</span></div><div>' + (Math.floor(diff / (1000 * 60 * 60)) - Math.floor(diff / (1000 * 60 * 60 * 24)) * 24) + '<span>Hours</span></div><div>' + (Math.floor(diff / (1000 * 60)) - Math.floor(diff / (1000 * 60 * 60)) * 60) + '<span>Minutes</span></div><div>' + (Math.floor(diff / 1000) - Math.floor(diff / (1000 * 60)) * 60) + '<span>Seconds</span></div>';
